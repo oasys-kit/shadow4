473452d41b92fba25014bd623a5aba7b483c6861
Manuel Sanchez del Rio <srio@esrf.eu>
2018-11-10 22:55:14 +0100

    prograssing with bm

--- minishadow/source_geometrical/probability_distributions.py
+++ minishadow/source_geometrical/probability_distributions.py
@@ -1,9 +1,6 @@
 
 import numpy
 from syned.syned_object import SynedObject
-from collections import OrderedDict
-
-
 
 
 class DistributionGeneric(SynedObject):
@@ -30,16 +27,32 @@ class Distribution2D(DistributionGeneric):
 
 class Distribution1D(DistributionGeneric):
     def __init__(self):
-        DistributionGeneric.__init__(self)
+        pass
         
     def get_dimension(self):
         return 1
 
-#
-# Subclasses for Distribution2D
-#
 
-class RectangleUniform2D(Distribution2D):
+#
+# Subclasses for Distribution2D used for spatial type sampling
+#  ["Point","Rectangle","Ellipse","Gaussian"]
+
+class Point2D(Distribution2D):
+    def __init__(self, h_center=0.0,v_center=0.0):
+        self._h_center = h_center
+        self._v_center = v_center
+
+        # support text containg name of variable, help text and unit. Will be stored in self._support_dictionary
+        self._set_support_text([
+                    ("h_center"         , "h (center) ", "" ),
+                    ("v_center"         , "v (center) ", "" ),
+            ] )
+
+    def get_sampled_points(self,N):
+        return numpy.zeros(N)+self._h_center,numpy.zeros(N)+self._v_center
+
+
+class Rectangle2D(Distribution2D):
     def __init__(self, h_min,h_max,v_min,v_max):
         self._h_min = h_min
         self._h_max = h_max
@@ -53,10 +66,174 @@ class RectangleUniform2D(Distribution2D):
                     ("v_max"         , "v (length) maximum (signed)  ", "" ),
             ] )
 
+        # return ["Point","Rectangle","Ellipse","Gaussian"]
+        # return ["Flat","Uniform","Gaussian","Conical"]
 
-class NumericalMesh2D(Distribution2D):
-    def __init__(self,function=None):
-        self.function = function
+    def get_sampled_points(self,N):
+        return Uniform1D.sample(N,self._h_min,self._h_max),Uniform1D.sample(N,self._v_min,self._v_max)
+
+    @classmethod
+    def sample(cls,N,h_min,h_max,v_min,v_max):
+        return Rectangle2D(h_min,h_max,v_min,v_max).get_sampled_points(N)
+
+
+class Ellipse2D(Distribution2D):
+    def __init__(self, h_min,h_max,v_min,v_max):
+        self._h_min = h_min
+        self._h_max = h_max
+        self._v_min = v_min
+        self._v_max = v_max
+        # support text containg name of variable, help text and unit. Will be stored in self._support_dictionary
+        self._set_support_text([
+                    ("h_min"         , "h (width) minimum (signed)   ", "" ),
+                    ("h_max"         , "h (width) maximum (signed)   ", "" ),
+                    ("v_min"         , "v (length) minimum (signed)  ", "" ),
+                    ("v_max"         , "v (length) maximum (signed)  ", "" ),
+            ] )
+
+        # return ["Point","Rectangle","Ellipse","Gaussian"]
+        # return ["Flat","Uniform","Gaussian","Conical"]
+
+    def get_sampled_points(self,N):
+        # ! C
+        # ! C Elliptical source **
+        # ! C Uses a transformation algorithm to generate a uniform variate distribution
+        # ! C
+        phi = numpy.pi * 2 * numpy.random.random(N)
+        radius = numpy.sqrt(numpy.random.random(N))
+        x = 0.5 *(self._h_max+self._h_min) + 0.5 * (self._h_max-self._h_min) * radius * numpy.cos(phi)
+        y = 0.5 *(self._v_max+self._v_min) + 0.5 * (self._v_max-self._v_min) * radius * numpy.sin(phi)
+        return x,y
+
+    @classmethod
+    def sample(cls,N,h_min,h_max,v_min,v_max):
+        return Ellipse2D(h_min,h_max,v_min,v_max).get_sampled_points(N)
+
+class Gaussian2D(Distribution2D):
+    def __init__(self, sigma_h,sigma_v):
+        self._sigma_h = sigma_h
+        self._sigma_v = sigma_v
+
+        # support text containg name of variable, help text and unit. Will be stored in self._support_dictionary
+        self._set_support_text([
+                    ("sigma_h"         , "h (width) sigma", "" ),
+                    ("sigma_v"         , "v (width) sigma", "" ),
+            ] )
+
+        # return ["Point","Rectangle","Ellipse","Gaussian"]
+        # return ["Flat","Uniform","Gaussian","Conical"]
+
+    def get_sampled_points(self,N):
+        return Gaussian1D.sample(N,self._sigma_h),Gaussian1D.sample(N,self._sigma_v)
+
+    @classmethod
+    def sample(cls,N,sigma_h,sigma_v):
+        return Gaussian2D(sigma_h,sigma_v).get_sampled_points(N)
+
+
+#
+# Subclasses for Distribution2D used for angle emission sampling
+#  ["Flat","Uniform","Gaussian","Conical"]
+#
+
+
+class Flat2D(Rectangle2D):
+    def __init__(self,h_min,h_max,v_min,v_max):
+        super().__init__(h_min,h_max,v_min,v_max)
+
+
+class Uniform2D(Distribution2D):
+    def __init__(self, h_min,h_max,v_min,v_max):
+        self._h_min = h_min
+        self._h_max = h_max
+        self._v_min = v_min
+        self._v_max = v_max
+        # support text containg name of variable, help text and unit. Will be stored in self._support_dictionary
+        self._set_support_text([
+                    ("h_min"         , "h (width) minimum (signed)   ", "" ),
+                    ("h_max"         , "h (width) maximum (signed)   ", "" ),
+                    ("v_min"         , "v (length) minimum (signed)  ", "" ),
+                    ("v_max"         , "v (length) maximum (signed)  ", "" ),
+            ] )
+
+        # return ["Point","Rectangle","Ellipse","Gaussian"]
+        # return ["Flat","Uniform","Gaussian","Conical"]
+
+    def get_sampled_points(self,N):
+        # ! C
+        # ! C   Uniform distribution ( Isotrope emitter )
+        # ! C
+        XMAX1 =   numpy.tan(self._h_min)
+        XMAX2 =   numpy.tan(self._h_max)
+        ZMAX1 =   numpy.tan(self._v_min)
+        ZMAX2 =   numpy.tan(self._v_max)
+        XRAND = numpy.random.random(N) * (XMAX1 - XMAX2) + XMAX2
+        ZRAND = numpy.random.random(N) * (ZMAX1 - ZMAX2) + ZMAX2
+        THETAR  = numpy.arctan(numpy.sqrt(XRAND**2+ZRAND**2))
+        PHIR = numpy.arctan2(ZRAND,XRAND)
+        DIREC1  = numpy.cos(PHIR) * numpy.sin(THETAR)
+        # DIREC2  = numpy.cos(THETAR)
+        DIREC3  = numpy.sin(PHIR) * numpy.sin(THETAR)
+        return DIREC1,DIREC3
+
+    @classmethod
+    def sample(cls,N,h_min,h_max,v_min,v_max):
+        return Uniform2D(h_min,h_max,v_min,v_max).get_sampled_points(N)
+
+
+
+
+class Conical2D(Distribution2D):
+    def __init__(self, cone_max=10e-6,cone_min=0.0):
+        self._cone_max = cone_max
+        self._cone_min = cone_min
+        # support text containg name of variable, help text and unit. Will be stored in self._support_dictionary
+        self._set_support_text([
+                    ("cone_max"         , "max angle for cone semiaperture  ", "" ),
+                    ("cone_min"         , "max angle for cone semiaperture  ", "" ),
+            ] )
+
+    def get_sampled_points(self,N):
+        # ! C   Now generates a set of rays along a cone centered about the normal,
+        # ! C   plus a ray along the normal itself.
+        # ! C
+        # IF (FGRID.EQ.1.OR.FGRID.EQ.3) THEN
+        #   ANGLE =   TWOPI*GRID(4,ITIK)*(IDO_VX-1)/IDO_VX
+        # ELSE
+        # ANGLE =   TWOPI*GRID(4,ITIK)
+        # END IF
+        # ! C temp fix -- 16 Jan 1987
+        # ! C      ANG_CONE =   CONE_MIN +
+        # ! C     $ (CONE_MAX - CONE_MIN)*GRID(6,ITIK)
+        # ANG_CONE =   COS(CONE_MIN) - GRID(6,ITIK)*(COS(CONE_MIN)-COS(CONE_MAX))
+        # ANG_CONE =  ACOS(ANG_CONE)
+        # DIREC(1) =   SIN(ANG_CONE)*COS(ANGLE)
+        # DIREC(2) =   COS(ANG_CONE)
+        # DIREC(3) =   SIN(ANG_CONE)*SIN(ANGLE)
+
+        ANGLE = 2 * numpy.pi * numpy.random.random(N)
+        ANG_CONE = numpy.cos(self._cone_min) - numpy.random.random(N) * \
+                                               (numpy.cos(self._cone_min)-numpy.cos(self._cone_max))
+        ANG_CONE = numpy.arccos(ANG_CONE)
+        DIREC1 = numpy.sin(ANG_CONE) * numpy.cos(ANGLE)
+        # DIREC2 = numpy.cos(ANG_CONE)
+        DIREC3 = numpy.sin(ANG_CONE) * numpy.sin(ANGLE)
+
+        return DIREC1,DIREC3
+
+    @classmethod
+    def sample(cls,N,cone_max=10e-6,cone_min=0.0):
+        return Conical2D(cone_max=cone_max,cone_min=cone_min).get_sampled_points(N)
+
+
+
+
+
+
+
+# class NumericalMesh2D(Distribution2D):
+#     def __init__(self,function=None):
+#         self.function = function
 
 
 #
@@ -66,7 +243,6 @@ class NumericalMesh2D(Distribution2D):
 
 class Uniform1D(Distribution1D):
     def __init__(self, x_min=-0.010, x_max=0.010):
-        super().__init__()
 
         self._x_min  = x_min
         self._x_max  = x_max
@@ -118,11 +294,11 @@ if __name__=="__main__":
     u = Uniform1D(-10,10)
     print(u.info())
 
-    u2 = RectangleUniform2D(-10,10,-5,5)
+    u2 = Uniform2D(-10,10,-5,5)
     print(u2.info())
 
     #
-    # Gaussian
+    # Gaussian1D
     #
     N = 1000
     g = Gaussian1D(sigma=0.25)
@@ -135,7 +311,7 @@ if __name__=="__main__":
         plot(numpy.arange(N),Gaussian1D.sample(sigma=0.25,N=N),title="Gaussian")
 
     #
-    # Uniform
+    # Uniform1D
     #
     N = 1000
     g = Uniform1D(10,20)
@@ -146,5 +322,57 @@ if __name__=="__main__":
     print(">>>>>",numpy.abs(sampled.mean() - 15 ))
     assert ( numpy.abs(sampled.mean() - 15 ) < 0.2)
 
+
     if do_plot:
         plot(numpy.arange(N),Uniform1D.sample(N,10,20))
+
+    #
+    # Elipse
+    #
+    N = 1000
+    g = Ellipse2D(-4,0,-3,-1)
+    x,y = g.get_sampled_points(N)
+    # plot_scatter(x,y,xrange=[-6,6],yrange=[-6,6])
+
+    #
+    # Rectangle2D
+    #
+
+    dx,dy = Rectangle2D.sample(5000,-2.5e-6,2.5e-6,-0.5e-6,0.5e-6,)
+    print(dx)
+    if do_plot:
+        plot_scatter(1e6*dx,1e6*dy,title="Rectangle")
+
+    #
+    # Uniform2D
+    #
+
+    dx,dy = Uniform2D.sample(5000,-2.5e-6,2.5e-6,-0.5e-6,0.5e-6,)
+    print(dx)
+    if do_plot:
+        plot_scatter(1e6*dx,1e6*dy,title="Uniform")
+
+    #
+    # Flat2D
+    #
+
+    dx,dy = Flat2D.sample(5000,-2.5e-6,2.5e-6,-0.5e-6,0.5e-6,)
+
+    if do_plot:
+        plot_scatter(1e6*dx,1e6*dy,title="Flat2D")
+
+    #
+    # Gaussian2D
+    #
+
+    dx,dy = Gaussian2D.sample(5000,2.5e-6,0.5e-6,)
+    if do_plot:
+        plot_scatter(1e6*dx,1e6*dy,title="Gaussian2D")
+
+    #
+    # Conical2D
+    #
+
+    dx,dy = Conical2D.sample(5000,2.5e-6,0.5e-6,)
+    if do_plot:
+        plot_scatter(1e6*dx,1e6*dy,title="Gaussian2D")
--- minishadow/source_geometrical/source_geometrical.py
+++ minishadow/source_geometrical/source_geometrical.py
@@ -1,5 +1,6 @@
 import numpy
-from probability_distributions import Uniform1D
+from probability_distributions import Rectangle2D, Ellipse2D, Gaussian2D
+from probability_distributions import Flat2D, Uniform2D, Conical2D
 
 
 class SourceGeometrical(object):
@@ -9,57 +10,241 @@ class SourceGeometrical(object):
                     energy_distribution = "Single line",
                  ):
 
-        self.spatial_type = spatial_type
-        self.angular_distribution = angular_distribution
-        self.energy_distribution = energy_distribution
+        self.set_spatial_type_by_name(spatial_type)
+        self.set_angular_distribution_by_name(angular_distribution)
+        # self.energy_distribution = energy_distribution
 
-        self.rays = None
+
 
     @classmethod
     def spatial_type_list(cls):
+        # fsour	= 0 - spatial source type/shape in X-Z plane.  Options are:
+        # 			  point (0), rectangle (1), ellipse (2), gaussian (3).
         return ["Point","Rectangle","Ellipse","Gaussian"]
+
+    def set_spatial_type_by_name(self,name):
+        if name == "Point":
+            self.set_spatial_type_point()
+        elif name == "Rectangle":
+            self.set_spatial_type_rectangle()
+        elif name == "Ellipse":
+            self.set_spatial_type_ellipse()
+        elif name == "Gaussian":
+            self.set_spatial_type_gaussian()
+        else:
+            raise Exception("Wrong spatial type: %s"%name)
+
+    def set_spatial_type_point(self):
+        self.spatial_type = "Point"
+
+    def set_spatial_type_rectangle(self,width=2.0,height=1.0):
+        # wxsou	=  0.0000000000000000E+00 - for fsour=1,2; source width (X).
+        # wzsou	=  0.0000000000000000E+00 - for fsour=1,2; source height (Z).
+        self.spatial_type = "Rectangle"
+        self.__wxsou = width
+        self.__wzsou = height
+
+    def set_spatial_type_ellipse(self,width=2.0,height=1.0):
+        # wxsou	=  0.0000000000000000E+00 - for fsour=1,2; source width (X).
+        # wzsou	=  0.0000000000000000E+00 - for fsour=1,2; source height (Z).
+        self.spatial_type = "Ellipse"
+        self.__wxsou = width
+        self.__wzsou = height
+
+    def set_spatial_type_gaussian(self,sigma_h=2.0,sigma_v=1.0):
+        # sigmax	=  0.0000000000000000E+00 - for fsour=3; sigma in X
+        # sigmaz	=  0.0000000000000000E+00 - for fsour=3; sigma in Z
+        self.spatial_type = "Gaussian"
+        self.__sigmax = sigma_h
+        self.__sigmaz = sigma_v
 
     @classmethod
     def angular_distribution_list(cls):
+         # fdistr	= 2 - defines source angle distribution types:
+         # 		  Available options are: flat(1),uniform(2),
+         # 		  gaussian(3), synchrotron(4), conical(5), exact
+         # 		  synchrotron(6).
         return ["Flat","Uniform","Gaussian","Conical"]
+
+        # cone_max	=  0.0000000000000000E+00 - for fdistr=5; maximum half
+        # 					    divergence.
+        # cone_min	=  0.0000000000000000E+00 - for fdistr=5; minimum half
+        # 					    divergence.
+        #
+        # sigdix	=  0.0000000000000000E+00 - for fdistr=3; sigma (radians)for horizontal
+        # 				    divergence (gaussian angle distribution).
+        # sigdiz	=  0.0000000000000000E+00 - for fdistr=3; sigma (radians) for vertical
+        # 				    divergence (gaussian angle distribution).
+        # hdiv1	=  0.0000000000000000E+00 - horizontal divergence in +X (radians).
+        # hdiv2	=  0.0000000000000000E+00 - horizontal divergence in -X (radians).
+        # vdiv1	=  6.0000000000000002E-05 - vertical divergence in +Z (radians).
+        # vdiv2	=  6.0000000000000002E-05 - vertical divergence in -Z (radians).
+
+    def set_angular_distribution_by_name(self,name):
+        if name == "Flat":
+            self.set_angular_distribution_flat()
+        elif name == "Uniform":
+            self.set_angular_distribution_uniform()
+        elif name == "Gaussian":
+            self.set_angular_distribution_gaussian()
+        elif name == "Conical":
+            self.set_angular_distribution_conical()
+        else:
+            raise Exception("Wrong angular distribution: %s"%name)
+
+
+
+    # WARNING: in shadow4 limits are signed!!!!
+    def set_angular_distribution_flat(self,hdiv1=-5e-6,hdiv2=5e-6,vdiv1=-0.5e-6,vdiv2=0.5e-6):
+        self.angular_distribution = "Flat"
+        self.__hdiv1 = hdiv1
+        self.__hdiv2 = hdiv2
+        self.__vdiv1 = vdiv1
+        self.__vdiv2 = vdiv2
+
+    # WARNING: in shadow4 limits are signed!!!!
+    def set_angular_distribution_uniform(self,hdiv1=-5e-6,hdiv2=5e-6,vdiv1=-0.5e-6,vdiv2=0.5e-6):
+        self.angular_distribution = "Uniform"
+        self.__hdiv1 = hdiv1
+        self.__hdiv2 = hdiv2
+        self.__vdiv1 = vdiv1
+        self.__vdiv2 = vdiv2
+
+    def set_angular_distribution_gaussian(self,sigdix=1e-6,sigdiz=1e-6):
+        self.angular_distribution = "Gaussian"
+        self.__sigdix = sigdix
+        self.__sigdiz = sigdiz
+
+    def set_angular_distribution_conical(self,cone_max=10e-6,cone_min=0.0):
+        self.angular_distribution = "Conical"
+        self.__cone_max = cone_max
+        self.__cone_min = cone_min
 
     @classmethod
     def energy_distribution_list(cls):
         return ["Single line","Several lines","Uniform","Relative intensities","Gaussian","User defined"]
 
-    def _sample_rays_default(self,N):
-        self.rays = numpy.zeros((N,18))
-        self.rays[:,4] = 1.0  # vy
-        self.rays[:,6] = 1.0  # Ex
-        self.rays[:,9] = 1.0  # flag
-        self.rays[:,10] = 2 * numpy.pi / (1e-10 * 100) # wavenumber
-        self.rays[:,11] = numpy.arange(N) + 1          # index
+    @classmethod
+    def _sample_rays_default(cls,N=5000):
+        rays = numpy.zeros((N,18))
+        rays[:,4] = 1.0  # vy
+        rays[:,6] = 1.0  # Ex
+        rays[:,9] = 1.0  # flag
+        rays[:,10] = 2 * numpy.pi / (1e-10 * 100) # wavenumber
+        rays[:,11] = numpy.arange(N) + 1          # index
+        return rays
 
-    def calculate_rays(self,N=1000):
+    def calculate_rays(self,N=5000):
 
-        self._sample_rays_default(N)
+        rays = self._sample_rays_default(N)
 
         print(">> Spatial type: %s"%(self.spatial_type))
 
+        #
+        # spatial type
+        #
         if self.spatial_type == "Point":
             pass
         elif self.spatial_type == "Rectangle":
-            self.rays[:,0] = Uniform1D.sample(N,-1.0,1.0)
-            self.rays[:,2] = Uniform1D.sample(N,-2.0,2.0)
+            rays[:,0],rays[:,2] = Rectangle2D.sample(N,
+                                    -0.5*self.__wxsou,
+                                    +0.5*self.__wxsou,
+                                    -0.5*self.__wzsou,
+                                    +0.5*self.__wzsou)
         elif self.spatial_type == "Ellipse":
-            raise Exception(NotImplemented)
+            rays[:,0],rays[:,2] = Ellipse2D.sample(N,
+                                    -0.5*self.__wxsou,
+                                    +0.5*self.__wxsou,
+                                    -0.5*self.__wzsou,
+                                    +0.5*self.__wzsou)
         elif self.spatial_type == "Gaussian":
-            raise Exception(NotImplemented)
+            rays[:,0],rays[:,2] = Gaussian2D.sample(N,
+                                    self.__sigmax,
+                                    self.__sigmaz)
         else:
             raise Exception("Bad value of spatial_type")
 
+        #
+        # angular distribution
+        #
+        print(">> Angular distribution: %s"%(self.angular_distribution))
+
+        if self.angular_distribution == "Flat":
+            rays[:,3],rays[:,5] = Flat2D.sample(N,
+                                    self.__hdiv1,
+                                    self.__hdiv2,
+                                    self.__vdiv1,
+                                    self.__vdiv2)
+            rays[:,4] = numpy.sqrt(-rays[:,3]**2 - rays[:,5]**2 + 1.0)
+        elif self.angular_distribution == "Uniform":
+            rays[:,3],rays[:,5] = Uniform2D.sample(N,
+                                    self.__hdiv1,
+                                    self.__hdiv2,
+                                    self.__vdiv1,
+                                    self.__vdiv2)
+            rays[:,4] = numpy.sqrt(-rays[:,3]**2 - rays[:,5]**2 + 1.0)
+        elif self.angular_distribution == "Gaussian":
+            rays[:,3],rays[:,5] = Gaussian2D.sample(N,
+                                    self.__sigdix,
+                                    self.__sigdiz)
+            rays[:,4] = numpy.sqrt(-rays[:,3]**2 - rays[:,5]**2 + 1.0)
+        elif self.angular_distribution == "Conical":
+            rays[:,3],rays[:,5] = Conical2D.sample(N,
+                                    self.__cone_max,
+                                    self.__cone_min)
+            rays[:,4] = numpy.sqrt(-rays[:,3]**2 - rays[:,5]**2 + 1.0)
+        else:
+            raise Exception("Bad value of angular_distribution")
+
+        return rays
 
 if __name__ == "__main__":
 
+    from srxraylib.plot.gol import plot,plot_scatter
 
-    gs = SourceGeometrical(spatial_type="Rectangle")
-    gs.calculate_rays(1000)
+    # rectangle
+    gs = SourceGeometrical()
+    gs.set_spatial_type_rectangle(4,2)
+    rays = gs.calculate_rays(1000)
 
     for i in range(6):
-        print("Limits for column %d : %f,%f"%(1+i,gs.rays[:,i].min(),gs.rays[:,i].max()))
+        print("Limits for column %d : %g,%g"%(1+i,rays[:,i].min(),rays[:,i].max()))
+    # plot_scatter(rays[:,0],rays[:,2],xrange=[-3,3],yrange=[-3,3])
 
+
+    # Gaussian
+    gs = SourceGeometrical()
+    gs.set_spatial_type_gaussian(2e-6,1e-6)
+    rays = gs.calculate_rays(1000)
+
+    for i in range(6):
+        print("Std for column %d : %g"%(i+1,rays[:,i].std()))
+
+    # Ellipse
+    gs = SourceGeometrical()
+    gs.set_spatial_type_ellipse(2,1)
+    rays = gs.calculate_rays(1000)
+    # plot_scatter(rays[:,0],rays[:,2],xrange=[-3,3],yrange=[-3,3])
+
+    # Uniform divergence
+    gs = SourceGeometrical()
+    print(">>>>",rays[:,3].shape)
+    gs.set_angular_distribution_uniform(-10e-6,5e-6,-3e-6,6e-6)
+    rays = gs.calculate_rays(5000)
+    # plot_scatter(1e6*rays[:,3],1e6*rays[:,5],xrange=[-10,10],yrange=[-10,10])
+
+    # Gaussian divergence
+    gs = SourceGeometrical()
+    print(">>>>",rays[:,3].shape)
+    gs.set_angular_distribution_gaussian(2e-5,1e-5)
+
+    rays = gs.calculate_rays(5000)
+    # plot_scatter(1e6*rays[:,3],1e6*rays[:,5],title="Gaussian div")
+
+    # Conical divergence
+    gs = SourceGeometrical()
+    print(">>>>",rays[:,3].shape)
+    gs.set_angular_distribution_conical(2e-5,1e-5)
+
+    rays = gs.calculate_rays(5000)
+    plot_scatter(1e6*rays[:,3],1e6*rays[:,5],title="Conical div")
