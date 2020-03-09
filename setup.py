f2f73eba7edcbd5c73072815228f841c11a1d884
Manuel Sanchez del Rio <srio@esrf.eu>
2020-03-09 15:48:11 -0700

    update

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ INSTALL_REQUIRES = (
 
 
 setup(name='shadow4',
-      version='0.0.13',
+      version='0.0.14',
       description='shadow implementation in python',
       author='Manuel Sanchez del Rio',
       author_email='srio@lbl.gov, srio@esrf.eu',
